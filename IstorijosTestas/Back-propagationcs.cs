d804bd69a6f9f31fff60ab1e53531919f4bc4546
Jurate Rackaityte <jurate.rackaityte@ktu.edu>
2017-06-05 12:45:48 +0300

    truputis daugiau idirbio prie Back-propagationcs.cs ir NeuralNetwork.cs Ivesti random duomenys, sukurti folder'iai, kur veliau manually idesim train ir test duomenis Vis dar yra klaidu, viena kodo vieta neuzbaigta

--- IstorijosTestas/Back-propagationcs.cs
+++ IstorijosTestas/Back-propagationcs.cs
@@ -3,22 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace IstorijosTestas
 {
     class Back_propagationcs
     {
-        int numInput = 4;       //jei nepridesim lyties: 4
-        int numHidden = 5;      //PAEKSPERIMENTUOTI kuris skaicius geriausiai tinka
-        public int numOutput = 9;      //tureti omeny, jog gali keistis
+
+        static int numInput = 4;       //jei nepridesim lyties: 4
+        static int numHidden = 5;      //PAEKSPERIMENTUOTI kuris skaicius geriausiai tinka
+        static public int numOutput = 9;      //tureti omeny, jog gali keistis
         int numRows = 1000;     //PALA, KA SITAI REISKIA?
 
-        double[][] trainData;       //VELIAU: PADARYTI TESTINIU DUOMENU NUSKAITYMA IS FAILO
-        double[][] testData;        //VELIAU: PADARYTI TESTINIU DUOMENU NUSKAITYMA IS FAILO
+        // veliau padaryti su 
+        // Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName
+        // kad ir kitiems automatiskai rastu failu vieta
+        string trainInputFolderPath = @"C:\Users\Jurate\Documents\GitHub\TextBasedGameWithoutGraphics\train data\input";
+        string trainOutputFolderPath = @"C:\Users\Jurate\Documents\GitHub\TextBasedGameWithoutGraphics\train data\output";
 
+        string testInputFolderPath = @"C:\Users\Jurate\Documents\GitHub\TextBasedGameWithoutGraphics\test data\input";
+        string testOutputFolderPath = @"C:\Users\Jurate\Documents\GitHub\TextBasedGameWithoutGraphics\test data\output";
+
+        int[][] trainData;       //VELIAU: PADARYTI TESTINIU DUOMENU NUSKAITYMA IS FAILO
+        int[][] testData;        //VELIAU: PADARYTI TESTINIU DUOMENU NUSKAITYMA IS FAILO
+		
         NeuralNetwork nn = new NeuralNetwork(numInput, numHidden, numOutput);
 
+        // uzpildom trainData ir testData masyvus
+        nn.readFiles(trainInputFolderPath, trainOutputFolderPath, out trainData, numInput, numOutput);
+
         int maxEpochs = 1000;       //PAEKSPERIMENTUOTI kuris skaicius geriausiai tinka
+        double learnRate = 0.05;    //PAEKSPERIMENTUOTI kuris skaicius geriausiai tinka
+        double momentum = 0.01;     //PAEKSPERIMENTUOTI kuris skaicius geriausiai tinka
+                                    // The momentum rate helps prevent training from getting stuck with local, non-optimal 
+                                    // weight values and also prevents oscillation where training never converges to stable values.
+
+        double[] weights = nn.TrainBackProp(trainData, maxEpochs, learnRate, momentum);
+
+        double trainAcc = nn.Accuracy(trainData);
+
+        double testAcc = nn.Accuracy(testData);
 
     }
+
 }
